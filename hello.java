c4dc8ad3e3fce5b5ab50e32a079b441422ed82c5
wude <wudemail@foxmail.com>
2023-03-13 17:07:20 +0800

    在小章鱼上操作编程并push：
    先创建一个仓库，我是同步到了本地和远程，再进入仓库文件夹下创建一个java文件，敲好后stage文件并写好描述就可以commit。注意这时只是本地仓库完成了提交，如果要远程提交到git上需要选中并push

---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * @FileName: hello
+ * @Author: wude
+ * @Date: 2023/3/13-16:45
+ * @Version: 1.0
+ * @Description: This is a java code
+ */
+
+public class hello {
+    public static void main(String[]args){
+        //这是一个输出语句
+        System.out.println("hello!");
+
+        }
+    }
