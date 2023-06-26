5ed662ae0dcc471ef75a1219b3cc13657380fa25
wude <wudemail@foxmail.com>
2023-06-26 22:40:21 +0800

    学习maven
    了解了maven的使用

---
+++ maven-project/java01/src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/**
+ * @BelongsProject: maven-project
+ * @BelongsPackage: com.itheima
+ * @FileName: HelloWorld
+ * @Author: wude
+ * @Date: 2023/6/26-20:39
+ * @Version: 1.0
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
